b75460a22049e92cf28184f4dc323633d78e9dc4
Florian Francis <florian.francis@icloud.com>
2026-08-20 13:33:05 +0530

    Fewchanges(ii)

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: './', // Ensures relative asset paths load correctly on Android mobile webviews
   plugins: [
     react(),
     VitePWA({
